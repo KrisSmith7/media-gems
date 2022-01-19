778cd97d36723b8a7e176b3f670d0f73fa6920a1
ekball <90882418+ekball@users.noreply.github.com>
2022-01-18 21:02:09 -0500

    added preliminary files to models, controllers, and db

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS media_gems_db;
+
+CREATE DATABASE media_gems_db;
--- models/index.js
+++ models/index.js
@@ -1,4 +1,7 @@
 // import all models
+const Reviews = require('./Review');
+const User = require('./User');
+const Services = require('./Service');
 
 // create associations
 
