4580d9164e1bfb031b8721b51c888d498ea38416
ekball <90882418+ekball@users.noreply.github.com>
2022-01-19 11:24:25 -0500

    added visited Model and edited info in models

--- models/index.js
+++ models/index.js
@@ -2,7 +2,16 @@
 const Reviews = require('./Review');
 const User = require('./User');
 const Services = require('./Service');
+const Visited = require('./Visited');
+
 
 // create associations
 
 // export models
+module.exports = {
+    Reviews,
+    User,
+    Services,
+    Visited,
+};
+  
