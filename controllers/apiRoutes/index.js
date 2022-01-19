dc45f7240a98988fb5b85c1cb20a7d61ec6328cd
Lisa Stillwell <lisastillwell@bellsouth.net>
2022-01-18 23:05:58 -0500

    #5 more updates for user-routes

--- controllers/apiRoutes/index.js
+++ controllers/apiRoutes/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
-// const userRoutes = require('./user-routes.');
+const userRoutes = require('./user-routes');
 const reviewRoutes = require('./review-Routes');
 
-
+router.use('/users', userRoutes);
 router.use('/reviews', reviewRoutes);
 
 
